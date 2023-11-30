4d3ad11cd16ec49cb42e7b5f53d7cc82e1219f62
HkHino <mpfugl@hotmail.com>
2023-11-30 12:07:06 +0100

    Add project files.

---
+++ ExamensProjekt checkout/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamensProjekt_checkout
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
